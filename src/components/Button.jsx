9db3261ebca3bae8014cc82fa62f022c6d5a34fe
mimu <akito.mimura@nijibox.co.jp>
2023-07-19 16:30:31 +0900

    add jest

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,19 @@
-import './button.css'
-import clsx from 'clsx'
+import "./button.css";
+import clsx from "clsx";
+import PropTypes from "prop-types";
 
-function Button({ children, color = 'default', size ='base' }) {
-  return <button className={clsx(color, size)}>{children}</button>;
+function Button({ children, color = "default", size = "base", handleClick }) {
+  return (
+    <button onClick={handleClick} className={clsx(color, size)}>
+      {children}
+    </button>
+  );
 }
 
 export default Button;
+
+Button.propTypes = {
+  color: PropTypes.oneOf(["primary", "default", "danger"]),
+  size: PropTypes.oneOf(["sm", "base", "lg"]),
+  handleClick: PropTypes.func.isRequired,
+};
--- src/components/Button.stories.jsx
+++ src/components/Button.stories.jsx
@@ -1,16 +1,73 @@
-import Button from './Button';
+import { expect } from "@storybook/jest";
+import Button from "./Button.jsx";
+import { linkTo } from "@storybook/addon-links";
+import { userEvent, within } from "@storybook/testing-library";
 
 export default {
-  title: 'Button',
   component: Button,
+  argTypes: {
+    handleClick: {
+      action: true,
+    },
+  },
+  parameters: {
+    backgrounds: {
+      values: [
+        { name: "red", value: "#f00" },
+        { name: "green", value: "#0f0" },
+        { name: "blue", value: "#00f" },
+      ],
+    },
+    layout: "centered",
+  },
+  // decorators: [
+  //   (Story) => (
+  //     <div
+  //       style={{
+  //         display: "flex",
+  //         justifyContent: "center",
+  //         alignItems: "center",
+  //         height: "100vh",
+  //       }}
+  //     >
+  //       <Story />
+  //     </div>
+  //   ),
+  // ],
 };
-export const Default = () => <Button>Default</Button>
-export const Primary = () => <Button color='primary'>Primary</Button>
-export const Danger = () => <Button color='danger'>Danger</Button>
+const Template = (args) => <Button {...args} />;
+export const Default = Template.bind({});
+Default.args = {
+  children: "Default",
+};
 
-export const PrimarySmall = () => (
-  <Button size="sm" color="primary">Small</Button>
-)
-export const PrimaryLarge = () => (
-  <Button size="lg" color="primary">Large</Button>
-)
+export const Danger = Template.bind({});
+Danger.args = {
+  children: "Danger",
+  color: "danger",
+};
+
+export const Primary = Template.bind({});
+Primary.args = {
+  children: "Primary",
+  color: "primary",
+};
+
+export const PrimarySmall = Template.bind({});
+PrimarySmall.args = {
+  children: "Small",
+  color: "primary",
+  size: "sm",
+};
+
+export const PrimaryLarge = Template.bind({});
+PrimaryLarge.args = {
+  children: "Large",
+  color: "primary",
+  size: "lg",
+};
+PrimaryLarge.play = async ({ args, canvasElement }) => {
+  const canvas = within(canvasElement);
+  await userEvent.click(canvas.getByRole("button"));
+  await expect(args.handleClick).toHaveBeenCalled();
+};
